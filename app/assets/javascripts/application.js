af27e84d1e4c03c84454d85efeb00777bee40163
Voon Shun Zhi <voonshunzhi@gmail.com>
2018-08-26 16:34:16 +0000

    Application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,7 @@
 
 $(document).ready(function(){
     let sidebar = $('#sidebar')
-        if(sidebar.height() < window.screen.height){
+        if(document.documentElement.height() < window.screen.height){
              sidebar.css('height','100vh')   
         }
 })
