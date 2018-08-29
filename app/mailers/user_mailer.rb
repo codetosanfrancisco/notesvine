955515c378ffd4f2017dce6f5922b8bece43e569
Voon Shun Zhi <voonshunzhi@gmail.com>
2018-08-29 16:23:26 +0000

    Done

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,7 @@ class UserMailer < ActionMailer::Base
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
     @user = user
+    attachments.inline['photo.png'] = File.read(File.join(Rails.root, 'app', 'assets', 'images','email_bg.jpg'))
     mail( :to => @user.email,
     :subject => 'Thanks for signing up for our amazing app' )
   end
